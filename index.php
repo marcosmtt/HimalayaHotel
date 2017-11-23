b51e534ef401f73fcfd25b6e9dfdc7f7dc1aa8fc
Marcos <marcos.mottabtmn@gmail.com>
2017-11-23 15:35:58 -0200

    first Commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <title>Himalaya Hotel</title>
+    <link rel="stylesheet" href="_css/headerNfooterStyle.css">
+  </head>
+  <body>
+    <header class="header">
+
+      <div class="headerContent">
+        <ul id="headerList">
+
+          <div class="logo">
+            <a href="index.php">
+              <img src="_images/logo.png" alt="logo" id="logo">
+            </a>
+          </div>
+
+          <li><a href="index.php">Home</a></li>
+          <li><a href="#">Cadastro</a></li>
+          <li><a href="#">Quartos</a></li>
+          <li><a href="#">Sobre</a></li>
+
+        </ul>
+      </div>
+
+    </header>
+
+    <div class="container">
+
+    </div>
+<footer class="footer">
+
+</footer>
+  </body>
+</html>
