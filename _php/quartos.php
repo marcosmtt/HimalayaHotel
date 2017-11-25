a363e5876b9dcb837a9640afebad07cdc5b54c0a
Marcos <marcos.mottabtmn@gmail.com>
2017-11-25 00:16:39 -0200

    Finish DB inserts

--- _php/quartos.php
+++ _php/quartos.php
@@ -28,20 +28,63 @@
 
     <div class="container">
       <table class="tabelaQuartos">
-        <tr id="tabHospedes">
-          <th>Número do Quarto</th>
-          <th>Hóspede Responsável</th>
-          <th>CPF</th>
-          <th>Número de Hóspedes</th>
-          <th>Diárias</th>
-          <th>Valor Total</th>
-        </tr>
-      </table>
+        <thead>
+          <tr id="tabHospedes">
+            <th>Número do Quarto</th>
+            <th>Hóspede Responsável</th>
+            <th>CPF</th>
+            <th>Número de Hóspedes</th>
+            <th>Diárias</th>
+            <th>Valor Total</th>
+          </tr>
+        </thead>
+      <tbody>
     </div>
 
     <footer class="footer">
     <p>Website Developed by Marcos Motta</p>
     </footer>
+
+<?php
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "hotel";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn->connect_error) {
+    die("Connectoin failed:" . $conn->connect_error);
+  }
+
+  $sql = "SELECT * FROM hospede";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+
+    while ($row = $result->fetch_assoc()) {
+      $numQuarto = $row["numeroQuarto"];
+      $nome = $row["nome"];
+      $cpf = $row["cpf"];
+      $numHospedes = $row["numero_de_Hospedes"];
+      $numDias = $row["numero_de_Dias"];
+      $valor = $row["valor"];
+
+      echo "<tr><td>" .$numQuarto."</td>";
+      echo "<td>" .$nome. "</td>";
+      echo "<td>" .$cpf. "</td>";
+      echo "<td>" .$numHospedes. "</td>";
+      echo "<td>" .$numDias. "</td>";
+      echo "<td>" .$valor. "</td></tr>";
+
+    }
+    echo "</tbody></tabel>";
+  }
+
+  $conn->close();
+
+ ?>
 
   </body>
 </html>
